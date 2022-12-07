3abbf376ed5e1e5f33aae06470766a126499343e
phanthanhluc24 <116275372+phanthanhluc24@users.noreply.github.com>
2022-12-07 17:30:36 +0700

    update and review

---
+++ data.js
@@ -0,0 +1,51 @@
+var producboy=[
+    {
+        anh:"https://tse2.mm.bing.net/th?id=OIP.rqMBRYDrkDKJ8Pd_rkn48QHaGC&pid=Api&P=0 " ,
+        ten:"The Cosmo (Đen) Quần short khaki",
+        gia:"250.000",
+        buy:"Đặt mua",
+        Phan_Loai:"Nam"
+    },
+    {
+        anh:"https://tse3.mm.bing.net/th?id=OIP.7bt760_qsQICPIVYhpA9kQHaHa&pid=Api&P=0 ",
+        ten:"Quần baggy den sang trọng QQ",
+        gia:"398.000",
+        buy:"Đặt mua",
+        Phan_Loai:"Nam"
+    },
+    {
+        anh:"https://tse4.mm.bing.net/th?id=OIP.4Gl6A9tG06sqBMV5vL6rrQHaHa&pid=Api&P=0",
+        ten:"The Cosmo (Đen) Quần short khaki",
+        gia:"300.000",
+        buy:"Đặt mua",
+        Phan_Loai:"Nam"
+    },
+    {
+        anh:"https://tse1.mm.bing.net/th?id=OIP.caK2GvLzBv4_flVtjf5JywHaHa&pid=Api&P=0",
+        ten:"The Cosmo (Đen) Quần short khaki",
+        gia:"300.000",
+        buy:"Đặt mua",
+        Phan_Loai:"Nam"
+    },
+    {
+        anh:"https://tse1.mm.bing.net/th?id=OIP.4OZ7znGRy3m3tTM6ERd4NAHaJQ&pid=Api&P=0",
+        ten:"Quần jean nữ - KO0002",
+        gia:"269.000vnđ   <del> 370.000 vnđ </del>",
+        buy:"Đặt mua",
+        Phan_Loai:"Nu"
+    },
+    {
+        anh:"https://tse4.mm.bing.net/th?id=OIP.Zu81Q6RsQHdQ_qn_WTuHQQHaHa&pid=Api&P=0",
+        ten:"Áo khoác nữ - VN0002",
+        gia:"249.000vnđ   <del> 369.000 vnđ </del>",
+        buy:"Đặt mua",
+        Phan_Loai:"Nu"
+    }
+    ,  {
+        anh:"https://tse3.mm.bing.net/th?id=OIP.bFDAUAM8TXeBoYwgfo6f_QHaHa&pid=Api&P=0",
+        ten:"Áo khoác nữ - VN0002",
+        gia:"269.000vnđ   <del> 370.000 vnđ </del>",
+        buy:"Đặt mua",
+        Phan_Loai:"Nu"
+    }
+]
